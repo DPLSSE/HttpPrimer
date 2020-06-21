6aa934ff6da0a1e23646d07b3e604db1e69a117f
Doug Durham <ddurham@dontpaniclabs.com>
2020-06-21 18:50:18 -0500

    Created Initial Page

---
+++ HttpPrimer/HttpPrimer/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HttpPrimer.Models;
+
+namespace HttpPrimer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string DoGet()
+        {
+            return Request.Query["FormInput"];
+        }
+
+        [HttpPost]
+        public string DoPost()
+        {
+            return Request.Form["FormInput"];
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HttpPrimer/HttpPrimer/Models/FormDataModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpPrimer.Models
+{
+    public class FormDataModel
+    {
+        public string FormInput;
+    }
+}
